9ad33c4a897f249a4b9390ad9df104a6f269ca05
Lucas Micheloni <lucas.michelonidesouza@gmail.com>
2020-01-06 17:14:15 -0300

    [GBA Memory] add half-word/word r/w

--- memory/src/memory.rs
+++ memory/src/memory.rs
@@ -16,27 +16,27 @@ pub mod sizes {
     pub const CART2_SIZE: usize = 0x200_0000;
     pub const CART_SRAM_SIZE: usize = 0x000_8000;
     pub const CART_FLASH512_SIZE: usize = 0x001_0000;
-    pub const CART_FLASH1M_SIZE: usize = 0x0020000;
-    pub const CART_EEPROM_SIZE: usize = 0x0002000;
-    pub const CART_EEPROM512_SIZE: usize = 0x0000200;
+    pub const CART_FLASH1M_SIZE: usize = 0x002_0000;
+    pub const CART_EEPROM_SIZE: usize = 0x000_2000;
+    pub const CART_EEPROM512_SIZE: usize = 0x000_0200;
 }
 
 pub mod base_addrs {
-    pub const BIOS_ADDR: usize = 0x0000000;
-    pub const WORKING_RAM_ADDR: usize = 0x2000000;
-    pub const WORKING_IRAM_ADDR: usize = 0x3000000;
-    pub const IO_REGISTERS_ADDR: usize = 0x4000000;
-    pub const PALETTE_RAM_ADDR: usize = 0x5000000;
-    pub const VRAM_ADDR: usize = 0x6000000;
-    pub const OAM_ADDR: usize = 0x7000000;
-    pub const CART0_ADDR: usize = 0x8000000;
-    pub const CART0_EX_ADDR: usize = 0x9000000;
-    pub const CART1_ADDR: usize = 0xA000000;
-    pub const CART1_EX_ADDR: usize = 0xB000000;
-    pub const CART2_ADDR: usize = 0xC000000;
-    pub const CART2_EX_ADDR: usize = 0xD000000;
-    pub const CART_SRAM_ADDR: usize = 0xE000000;
-    pub const CART_SRAM_MIRROR_ADDR: usize = 0xF000000;
+    pub const BIOS_ADDR: usize = 0x000_0000;
+    pub const WORKING_RAM_ADDR: usize = 0x200_0000;
+    pub const WORKING_IRAM_ADDR: usize = 0x300_0000;
+    pub const IO_REGISTERS_ADDR: usize = 0x400_0000;
+    pub const PALETTE_RAM_ADDR: usize = 0x500_0000;
+    pub const VRAM_ADDR: usize = 0x600_0000;
+    pub const OAM_ADDR: usize = 0x700_0000;
+    pub const CART0_ADDR: usize = 0x800_0000;
+    pub const CART0_EX_ADDR: usize = 0x900_0000;
+    pub const CART1_ADDR: usize = 0xA00_0000;
+    pub const CART1_EX_ADDR: usize = 0xB00_0000;
+    pub const CART2_ADDR: usize = 0xC00_0000;
+    pub const CART2_EX_ADDR: usize = 0xD00_0000;
+    pub const CART_SRAM_ADDR: usize = 0xE00_0000;
+    pub const CART_SRAM_MIRROR_ADDR: usize = 0xF00_0000;
 }
 
 pub struct MMU {
@@ -63,47 +63,74 @@ impl MMU {
     }
 
     /// Reads a byte from memory
-    pub fn rb(&self, addr: u32) -> u8 {
+    pub fn load8(&self, addr: u32) -> u8 {
         match addr as usize {
-            base_addrs::WORKING_RAM_ADDR..=0x0203FFFF => {
+            base_addrs::WORKING_RAM_ADDR..=0x0203_FFFF => {
                 self.wram[addr as usize - base_addrs::WORKING_RAM_ADDR]
             }
-            base_addrs::WORKING_IRAM_ADDR..=0x03007FFF => {
+            base_addrs::WORKING_IRAM_ADDR..=0x0300_7FFF => {
                 self.iwram[addr as usize - base_addrs::WORKING_RAM_ADDR]
             }
-            base_addrs::IO_REGISTERS_ADDR..=0x08000000 => self.gpu_mem.read(addr),
-            base_addrs::CART0_ADDR..=0x0DFFFFFF => self.rom[addr as usize - base_addrs::CART0_ADDR],
-            base_addrs::CART_SRAM_ADDR..=0x0E00FFFF => 0, // SRAM unimplemented
-            base_addrs::CART_SRAM_MIRROR_ADDR..=0x0F00FFFF => 0, // SRAM unimplemented
+            base_addrs::IO_REGISTERS_ADDR..=0x07FF_FFFF => self.gpu_mem.read(addr),
+            base_addrs::CART0_ADDR..=0x0DFF_FFFF => {
+                self.rom[addr as usize - base_addrs::CART0_ADDR]
+            }
+            base_addrs::CART_SRAM_ADDR..=0x0E00_FFFF => 0, // SRAM unimplemented
+            base_addrs::CART_SRAM_MIRROR_ADDR..=0x0F00_FFFF => 0, // SRAM unimplemented
             _ => 0,
         }
     }
 
+    /// Reads a half-word from memory
+    pub fn load16(&self, addr: u32) -> u16 {
+        let mut x = self.load8(addr) as u16;
+        x <<= 8;
+        x += self.load8(addr + 1) as u16;
+
+        x
+    }
+
+    /// Reads a byte from memory
+    pub fn load32(&self, addr: u32) -> u32 {
+        let mut x = self.load16(addr) as u32;
+        x <<= 16;
+        x += self.load16(addr + 2) as u32;
+
+        x
+    }
+
     /// Write a byte into memory
-    pub fn wb(&mut self, addr: u32, val: u8) {
+    pub fn store8(&mut self, addr: u32, val: u8) {
         match addr as usize {
             0x0..=base_addrs::BIOS_ADDR => self.bios[addr as usize] = val,
-            base_addrs::WORKING_RAM_ADDR..=0x0203FFFF => {
+            base_addrs::WORKING_RAM_ADDR..=0x0203_FFFF => {
                 self.wram[addr as usize - base_addrs::WORKING_RAM_ADDR] = val
             }
-            base_addrs::WORKING_IRAM_ADDR..=0x03007FFF => {
+            base_addrs::WORKING_IRAM_ADDR..=0x0300_7FFF => {
                 self.iwram[addr as usize - base_addrs::WORKING_IRAM_ADDR] = val
             }
-            base_addrs::IO_REGISTERS_ADDR..=0x08000000 => self.gpu_mem.write(addr, val),
-            base_addrs::CART0_ADDR..=0x0DFFFFFF => (), // writing in ROM
-            base_addrs::CART_SRAM_ADDR..=0x0E00FFFF => (), // SRAM unimplemented
-            base_addrs::CART_SRAM_MIRROR_ADDR..=0x0F00FFFF => (),
+            base_addrs::IO_REGISTERS_ADDR..=0x07FF_FFFF => self.gpu_mem.write(addr, val),
+            base_addrs::CART0_ADDR..=0x0DFF_FFFF => (), // writing in ROM
+            base_addrs::CART_SRAM_ADDR..=0x0E00_FFFF => (), // SRAM unimplemented
+            base_addrs::CART_SRAM_MIRROR_ADDR..=0x0F00_FFFF => (),
 
             _ => {}
         }
     }
 
+    /// Write an aligned half-word into memory
+    #[allow(overflowing_literals)]
+    pub fn store16(&mut self, addr: u32, val: u32) {
+        self.store8(addr, val as u8);
+        self.store8(addr + 1, (val >> 8) as u8);
+    }
+
     /// Write an aligned word into memory
     #[allow(overflowing_literals)]
-    pub fn ww(&mut self, addr: u32, val: u32) {
-        self.wb(addr, val as u8);
-        self.wb(addr + 1, (val >> 8) as u8);
-        self.wb(addr + 2, (val >> 16) as u8);
-        self.wb(addr + 3, (val >> 24) as u8);
+    pub fn store32(&mut self, addr: u32, val: u32) {
+        self.store8(addr, val as u8);
+        self.store8(addr + 1, (val >> 8) as u8);
+        self.store8(addr + 2, (val >> 16) as u8);
+        self.store8(addr + 3, (val >> 24) as u8);
     }
 }
