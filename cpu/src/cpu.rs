ac291c205d1d7454d46f19ac2315e53ef6473872
Guilherme Torres <guilhermetorres97@gmail.com>
2020-01-07 18:40:54 -0300

    making unit tests, default cpu, memory is overflowing stack

--- cpu/src/cpu.rs
+++ cpu/src/cpu.rs
@@ -1,5 +1,9 @@
 use memory::memory::MMU;
+use std::fs::File;
+use std::io::{Read, Result};
+use std::default::Default;
 mod enums;
+mod constants;
 
 pub struct CPU {
     mmu: MMU,
@@ -8,15 +12,30 @@ pub struct CPU {
     spsr: u32
 }
 
-fn run_rom(cpu: &mut CPU, rom_path: &String) {
-
+impl Default for CPU {
+    fn default() -> CPU {
+        CPU{
+            mmu: MMU::new(constants::default_cpu::MMU_DISPLAY).unwrap(),
+            registers: constants::default_cpu::RS,
+            cpsr: constants::default_cpu::CPSR,
+            spsr: constants::default_cpu::SPSR
+        }
+    }
 }
 
-fn read_rom_to_memory(cpu: &mut CPU, rom_path: &String) {
-
+pub fn run_rom(cpu: &mut CPU, rom_path: &str) {
+    let rom: Vec<u8> = read_rom_to_memory(rom_path).unwrap();
+    println!("{:?}", rom);
 }
 
-fn fetch(cpu: &mut CPU, rom: &Vec<u32>) {
+fn read_rom_to_memory(rom_path: &str) -> Result<Vec<u8>> {
+    let mut file = File::open(rom_path)?;
+    let mut rom = Vec::new();
+    file.read_to_end(&mut rom)?;
+    return Ok(rom);
+}
+
+fn fetch(cpu: &mut CPU, rom: &Vec<u8>) {
 
 }
 
