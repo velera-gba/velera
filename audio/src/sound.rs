127c8e9c920692519f0715fb44bb95be5fb33402
Kazuna Nakama <kazunanakama@gmail.com>
2020-02-18 02:56:18 -0600

    Got a Pulse to play!

--- audio/src/sound.rs
+++ audio/src/sound.rs
@@ -28,22 +28,25 @@ impl GbaAudio {
 
     pub fn test_tone(&self, freq: u32, channel: usize) {
         let sine = rodio::source::SineWave::new(freq);
-        let stream = &self.Channels[channel];
-        let mut noise = rand::random::<i16>();
+        let pulseA = &self.Channels[0];
+        let pulseB = &self.Channels[1];
+        let noise = &self.Channels[3];
+        let mut noise_sample = rand::random::<i16>();
         let mut time = 0;
-        print!("{}", noise);
+        print!("::{:?}", noise_sample);
         loop{
             time += 1;
-            noise = rand::random::<i16>();
+            noise_sample = rand::random::<i16>();
             //print!("::{}", &noise);
             let sample1 = StaticSamplesBuffer::new(1, 44100, &[1i16, -2048, -2048,-2048, -2048, -2048, -2048,-2048, -2048, -2048, -2048,-2048, -2048, -2048, -2048,-2048, -2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048]);
             let sample2 = StaticSamplesBuffer::new(1, 44100, &[1i16, -2048, -2048,-2048, -2048, -2048, -2048,-2048, -2048, -2048, -2048,-2048, -2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048]);
-            let sample3 = SamplesBuffer::new(1, 44100, vec!(1i16, noise));
+            let sample3 = SamplesBuffer::new(1, 44100, vec!(1i16, noise_sample));
             
-            stream.append(sample1);
-            stream.append(sample2);
-            stream.sleep_until_end();
-            //stream.append(sample2);
+            pulseA.append(sample1);
+            pulseA.sleep_until_end();
+            pulseB.append(sample2);
+            pulseB.sleep_until_end();
+            //noise.append(sample3);
             //stream.sleep_until_end();
             //   stream.append(repeat);
             //stream.sleep_until_end();
