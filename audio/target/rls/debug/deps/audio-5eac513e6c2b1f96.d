09c4cd26e169dc1f496bcde055f47741c3a155c4
Lucas Micheloni <lucas.michelonidesouza@gmail.com>
2020-02-19 22:17:41 -0300

    remove audio/main.rs

--- audio/target/rls/debug/deps/audio-5eac513e6c2b1f96.d
+++
@@ -1,5 +0,0 @@
-/home/kazuna/Velera[dev-null]/velera/audio/target/rls/debug/deps/audio-5eac513e6c2b1f96.rmeta: audio/src/lib.rs
-
-/home/kazuna/Velera[dev-null]/velera/audio/target/rls/debug/deps/audio-5eac513e6c2b1f96.d: audio/src/lib.rs
-
-audio/src/lib.rs:
